b12a8b0abb7412afd42763e0e603fbd4e3d0726b
Nicolas Galler <ngaller@gmail.com>
2017-06-06 17:05:38 -0400

    styling

--- example/index.js
+++ example/index.js
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom'
 import App from './App.jsx'
 
 const div = document.createElement('div')
+div.className = 'outer'
 document.body.appendChild(div)
 ReactDOM.render(<App />, div)
--- src/Calendar.js
+++ src/Calendar.js
@@ -8,6 +8,7 @@ import styled from 'styled-components'
 const FlexColumn = styled.div`
   display: flex;
   flex-direction: column;
+  height: 100%;
 `
 
 const HeaderStyle = styled.div`
